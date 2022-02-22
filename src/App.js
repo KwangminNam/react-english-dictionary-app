8bdc62df8b1da352d2b879393600655d8c4bf59a
KwangminNam <johnpower@naver.com>
2022-02-22 20:40:30 +0900

    c

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React121212
         </a>
       </header>
     </div>
