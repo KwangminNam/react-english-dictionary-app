cb1ec54d9574f7515f0dc34d59132790631a085f
KwangminNam <johnpower@naver.com>
2022-03-01 18:39:39 +0900

    map()

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import './App.css';
 import './component/Header';
 import Header from './component/Header';
 import DayList from './component/DayList';
+import Day from './component/Day';
+import {BrowserRouter,Route,Switch} from 'react-router-dom';
+
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <Header></Header>
-      <DayList></DayList>
+      <Header/>
+      <Switch>
+        <Route path="/">
+          
+        </Route>
+        <DayList/>
+        <Day/>
+      </Switch>
     </div>
+    </BrowserRouter>
   );
 }
 
